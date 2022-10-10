85cdddc3945da3f655025a9e3a623cfefaf88c20
Felipe Messa <messafelipe@gmail.com>
2022-10-10 15:55:44 -0300

    Começando a implementar o tabuleiro

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -7,9 +7,7 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            Posicao p = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + p);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
             Console.ReadLine();
         }
